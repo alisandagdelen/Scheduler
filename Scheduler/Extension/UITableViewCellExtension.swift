29cc1f1e9fb44ff12747da218558341bc63976c4
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-02-17 20:20:08 +0300

    - TableViewCell extension created

---
+++ Scheduler/Extension/UITableViewCellExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCellExtension.swift
+//  Scheduler
+//
+//  Created by alisandagdelen on 17.02.2018.
+//  Copyright © 2018 alisandagdelen. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var nibName: String {
+        return String(describing: self)
+    }
+}
