6f5972bfc31577c7dd95016afae56d4068dba5d9
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-02-17 16:58:43 +0300

    - ScheduleViewModel created

--- Scheduler/ViewModel/ScheduleViewModel.swift
+++ Scheduler/ViewModel/ScheduleViewModel.swift
@@ -14,9 +14,46 @@ protocol ScheduleViewModelProtocol {
     var endDate: Dynamic<Date> { get }
     var frequency: Dynamic<Frequency> { get }
     
-    func updateEndDate()
-    func updateBeginDate()
-    func updateFrequency()
+    func updateBeginDate(_ beginDate:Date)
+    func updateEndDate(_ endDate:Date)
+    func updateFrequency(_ frequency:Frequency)
 }
 
-
+class ScheduleViewModel: NSObject, ScheduleViewModelProtocol {
+    var schedule: Schedule {
+        didSet {
+            self.beginDate.value = self.schedule.beginDate
+            self.endDate.value = self.schedule.endDate
+            self.frequency.value = self.schedule.frequency
+        }
+    }
+    
+    var beginDate: Dynamic<Date>
+    
+    var endDate: Dynamic<Date>
+    
+    var frequency: Dynamic<Frequency>
+    
+    init(schedule: Schedule? = nil, beginDate:Date, endDate:Date, frequency:Date) {
+        self.schedule = schedule ?? Schedule(beginDate: Date(), frequency: .once, endDate: Date())
+        self.beginDate = Dynamic(self.schedule.beginDate)
+        self.endDate = Dynamic(self.schedule.endDate)
+        self.frequency = Dynamic(self.schedule.frequency)
+    }
+    
+    func updateBeginDate(_ beginDate:Date) {
+        self.schedule.beginDate = beginDate
+    }
+    
+    func updateEndDate(_ endDate:Date) {
+        self.schedule.endDate = endDate
+    }
+    
+    func updateFrequency(_ frequency:Frequency) {
+        self.schedule.frequency = frequency
+    }
+    
+    private func controlEndDate() {
+        
+    }
+}
