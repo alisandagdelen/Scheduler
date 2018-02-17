d33aca6aef4421179dcdf8b458dbcc9ab4a78f09
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-02-17 18:38:12 +0300

    - ScheduleVC created

---
+++ Scheduler/ViewController/ScheduleVC.swift
@@ -0,0 +1,24 @@
+//
+//  ScheduleVC.swift
+//  Scheduler
+//
+//  Created by alisandagdelen on 17.02.2018.
+//  Copyright © 2018 alisandagdelen. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    
+}
