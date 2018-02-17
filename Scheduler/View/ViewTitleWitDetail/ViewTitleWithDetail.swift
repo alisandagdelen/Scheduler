c8abf05a9950aa15d5c081fcead8a633f51515a4
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-02-17 23:20:15 +0300

    - ViewWithDetails view added with xib

---
+++ Scheduler/View/ViewTitleWitDetail/ViewTitleWithDetail.swift
@@ -0,0 +1,28 @@
+//
+//  ViewTitleWithDetail.swift
+//  Scheduler
+//
+//  Created by alisandagdelen on 17.02.2018.
+//  Copyright © 2018 alisandagdelen. All rights reserved.
+//
+
+import UIKit
+
+class ViewTitleWithDetail: UIView {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblDetail: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func loadNib(){
+        
+    }
+   
+}
