af77c16861429eecc17d1fbd0f68e2740de65bd3
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-02-17 20:19:36 +0300

    - TCellSchedule created

---
+++ Scheduler/View/TCellSchedule/TCellSchedule.swift
@@ -0,0 +1,24 @@
+//
+//  TCellSchedule.swift
+//  Scheduler
+//
+//  Created by alisandagdelen on 17.02.2018.
+//  Copyright © 2018 alisandagdelen. All rights reserved.
+//
+
+import UIKit
+
+class TCellSchedule: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
